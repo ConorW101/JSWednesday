4fa1c9a315944ec63b624ae1c1deb151ba27ee41
conor.wiltshire1992 <conor.wiltshire1992@gmail.com>
2022-02-02 10:31:14 +0000

    Functions JS

---
+++ functions.js
@@ -0,0 +1,19 @@
+function subCalc(num1, num2) {
+    return num1 - num2;
+}
+
+console.log(subCalc(50,28));
+
+
+
+const welcome = function(name,age,gender){
+    return console.log(`My name is ${name}, i am ${age} years old and of gender ${gender}`);
+    }
+   
+welcome("Conor Wiltshire",28,"Male");
+
+
+
+powerUp = (n1,n2) => Math.pow(n1,n2);
+
+console.log(powerUp(5,3));
